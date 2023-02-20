cce128fe0f1266b0b86884df4f3d41cbf7ab754d
frankribera <frankrr136@gmail.com>
2023-02-21 00:29:19 +0100

    Ya funciona el register con comprobacion de email

--- app/src/main/java/com/example/diariodetradingapp/RegisterActivity.java
+++ app/src/main/java/com/example/diariodetradingapp/RegisterActivity.java
@@ -1,10 +1,8 @@
 package com.example.diariodetradingapp;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +11,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.diariodetradingapp.modelos.Constantes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -55,6 +52,8 @@ public class RegisterActivity extends AppCompatActivity {
                     if (!txtEmail.getText().toString().isEmpty() && !txtPassword.getText().toString().isEmpty()){
                         doRegister(email,txtPassword.getText().toString());
                     }
+                }else{
+                    Toast.makeText(RegisterActivity.this, R.string.incorrect_email_or_password, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -68,36 +67,17 @@ public class RegisterActivity extends AppCompatActivity {
                         if (task.isSuccessful()){
                             SignInMethodQueryResult result = task.getResult();
                             if (result != null && result.getSignInMethods() != null && result.getSignInMethods().size() > 0){
-                                //alertRegister(Constantes.TITULO_ERROR, Constantes.EMAIL_EXISTE).show();
-                                Toast.makeText(RegisterActivity.this, "Este email ya esta en uso", Toast.LENGTH_LONG).show();
+                                Toast.makeText(RegisterActivity.this, R.string.email_in_use, Toast.LENGTH_LONG).show();
                             }else{
-                                //alertRegister(Constantes.TITULO_EXITO, Constantes.EMAIL_INEXISTE).show();
-                                Toast.makeText(RegisterActivity.this, "Cuenta creada", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RegisterActivity.this, R.string.account_created, Toast.LENGTH_SHORT).show();
                             }
                         }else{
-                            //alertRegister(Constantes.TITULO_ERROR,Constantes.ERROR_CONSULTA).show();
-                            Toast.makeText(RegisterActivity.this, "Error en la consulta, Fallo de la app", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, R.string.query_error, Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
 
-    private AlertDialog alertRegister(String tituloError, String emailExiste) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(tituloError);
-        builder.setMessage(emailExiste);
-        builder.setCancelable(false);
-
-        builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        return builder.create();
-
-    }
 
 
     private void doRegister(String email, String password) {
