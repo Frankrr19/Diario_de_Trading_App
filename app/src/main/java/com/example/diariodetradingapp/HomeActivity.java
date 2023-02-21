a45cc75ca5b0d2511eeaa192f64a4278edacc574
frankribera <frankrr136@gmail.com>
2023-02-21 19:04:20 +0100

    Ya funciona el guardar datos personales

--- app/src/main/java/com/example/diariodetradingapp/HomeActivity.java
+++ app/src/main/java/com/example/diariodetradingapp/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.diariodetradingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.btnConfigUser) {
+            startActivity(new Intent(HomeActivity.this, ConfigDatosPersonalesActivity.class));
+            finish();
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/diariodetradingapp/LoginActivity.java
+++ app/src/main/java/com/example/diariodetradingapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.diariodetradingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +24,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText txtPassword;
     private Button btnLogin;
     private TextView lblSignUp;
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,53 @@ public class LoginActivity extends AppCompatActivity {
         initializeComponents();
 
         goRegister();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = txtEmail.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                if (email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Rellena los datos", Toast.LENGTH_SHORT).show();
+                }else{
+                    doLogin(email, password);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = auth.getCurrentUser();
+        updateUI(user);
+    }
+
+    private void doLogin(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            user = auth.getCurrentUser();
+                            updateUI(user);
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null){
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            finish();
+        }
     }
 
     private void goRegister() {
@@ -41,5 +100,6 @@ public class LoginActivity extends AppCompatActivity {
         txtPassword = findViewById(R.id.txtPasswordLoginActivity);
         btnLogin = findViewById(R.id.btnLoginLoginActivity);
         lblSignUp = findViewById(R.id.lblSignUpLoginActivity);
+        auth = FirebaseAuth.getInstance();
     }
 }
