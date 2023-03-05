cd6501da86f0af343443d2c9214c596339245d68
frankribera <frankrr136@gmail.com>
2023-03-05 21:19:10 +0100

    ya permite leer los datos personales mas comienzo diseño del home ademas de implementacion del onBackPressed en algunas actividades y confirmacion de cerrar app, home hecho, scroll añadiado al home, añadido crear trade, ya crea trades la app

--- app/src/main/java/com/example/diariodetradingapp/CreateTradeActivity.java
+++ app/src/main/java/com/example/diariodetradingapp/CreateTradeActivity.java
@@ -1,24 +1,87 @@
 package com.example.diariodetradingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.diariodetradingapp.databinding.ActivityCreateTradeBinding;
-import com.example.diariodetradingapp.databinding.ActivityHome2Binding;
+import com.example.diariodetradingapp.modelos.Trade;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class CreateTradeActivity extends AppCompatActivity {
 
     private ActivityCreateTradeBinding binding;
+    private ArrayList<Trade> trades;
+    private Toolbar cabecera;
+    private FirebaseDatabase database;
+    private DatabaseReference refUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCreateTradeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        
+
+        cabecera = findViewById(R.id.cabecera);
+        setSupportActionBar(cabecera);
+
+        trades = new ArrayList<>();
+        database = FirebaseDatabase.getInstance("https://bd-diariotrading-default-rtdb.europe-west1.firebasedatabase.app/");
+        refUser = database.getReference(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("lista_trades");
+
         initializaSpinners();
+
+        binding.btnCreateCreateTrade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crearTrade();
+            }
+        });
+    }
+
+    private void crearTrade() {
+        if (binding.spEstateCreateTrade.getSelectedItem() != "" ||
+            binding.spEntryCreateTrade.getSelectedItem() != "" ||
+            binding.spYearCreateTrade.getSelectedItem() != "" ||
+            binding.spDayCreateTrade.getSelectedItem() != "" ||
+            binding.spMonthCreateTrade.getSelectedItem() != "" ||
+            binding.spMarketCreateTrade.getSelectedItem() != "" ||
+            !binding.txtContractsCreateTrade.getText().toString().isEmpty() ||
+            !binding.txtPointValueCreateTrade.getText().toString().isEmpty() ||
+            !binding.txtPointsCreateTrade.getText().toString().isEmpty() ||
+            binding.spEmotionCreateTrade.getSelectedItem() != ""){
+            try {
+                String state = binding.spEstateCreateTrade.getSelectedItem().toString();
+                String entry = binding.spEntryCreateTrade.getSelectedItem().toString();
+                int year = Integer.parseInt(binding.spYearCreateTrade.getSelectedItem().toString());
+                int day = Integer.parseInt(binding.spDayCreateTrade.getSelectedItem().toString());
+                String month = binding.spMonthCreateTrade.getSelectedItem().toString();
+                String market = binding.spMarketCreateTrade.getSelectedItem().toString();
+                float contracts = Float.parseFloat(binding.txtContractsCreateTrade.getText().toString());
+                float pointValue = Float.parseFloat(binding.txtPointValueCreateTrade.getText().toString());
+                float points = Float.parseFloat(binding.txtPointsCreateTrade.getText().toString());
+                String emotion = binding.spEmotionCreateTrade.getSelectedItem().toString();
+
+                Trade trade = new Trade(state, entry, year, day, month, market, contracts, pointValue, points, emotion);
+                trades.add(trade);
+                refUser.setValue(trades);
+                startActivity(new Intent(CreateTradeActivity.this, HomeActivity.class));
+                finish();
+            }catch (Exception e){
+                Toast.makeText(this, "Rellena todos los campos", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void initializaSpinners() {
@@ -45,6 +108,22 @@ public class CreateTradeActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.btnConfigUser) {
+            startActivity(new Intent(CreateTradeActivity.this, ConfigDatosPersonalesActivity.class));
+            finish();
+        }
+        return true;
+    }
     @Override
     public void onBackPressed() {
         startActivity(new Intent(CreateTradeActivity.this, HomeActivity.class));
--- app/src/main/java/com/example/diariodetradingapp/modelos/Trade.java
+++ app/src/main/java/com/example/diariodetradingapp/modelos/Trade.java
@@ -1,5 +1,117 @@
 package com.example.diariodetradingapp.modelos;
 
 public class Trade {
+    private String state;
+    private String entry;
+    private int year;
+    private int day;
+    private String month;
+    private String market;
+    private Float contracts;
+    private Float pointValue;
+    private Float points;
+    private String emotion;
+    private Float total;
 
+    public Trade(String state, String entry, int year, int day, String month, String market, Float contracts, Float pointValue, Float points, String emotion) {
+        this.state = state;
+        this.entry = entry;
+        this.year = year;
+        this.day = day;
+        this.month = month;
+        this.market = market;
+        this.contracts = contracts;
+        this.pointValue = pointValue;
+        this.points = points;
+        this.emotion = emotion;
+        this.total = this.contracts * this.pointValue * this.points;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEntry() {
+        return entry;
+    }
+
+    public void setEntry(String entry) {
+        this.entry = entry;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+
+    public Float getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(Float contracts) {
+        this.contracts = contracts;
+    }
+
+    public Float getPointValue() {
+        return pointValue;
+    }
+
+    public void setPointValue(Float pointValue) {
+        this.pointValue = pointValue;
+    }
+
+    public Float getPoints() {
+        return points;
+    }
+
+    public void setPoints(Float points) {
+        this.points = points;
+    }
+
+    public String getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(String emotion) {
+        this.emotion = emotion;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
 }
