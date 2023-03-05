3cdeca79f1e6e3c2f420f96c034c20028d7bbc72
frankribera <frankrr136@gmail.com>
2023-03-05 19:03:20 +0100

    ya permite leer los datos personales mas comienzo diseño del home ademas de implementacion del onBackPressed en algunas actividades y confirmacion de cerrar app, home hecho, scroll añadiado al home, añadido crear trade

--- app/src/main/java/com/example/diariodetradingapp/CreateTradeActivity.java
+++ app/src/main/java/com/example/diariodetradingapp/CreateTradeActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.diariodetradingapp.databinding.ActivityCreateTradeBinding;
+import com.example.diariodetradingapp.databinding.ActivityHome2Binding;
+
 public class CreateTradeActivity extends AppCompatActivity {
 
+    private ActivityCreateTradeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_trade);
+        binding = ActivityCreateTradeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     @Override
