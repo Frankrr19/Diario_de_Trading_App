30f35d99d03a01c5761bb89ed8e68a41908d28be
frankribera <frankrr136@gmail.com>
2023-03-05 19:42:54 +0100

    ya permite leer los datos personales mas comienzo diseño del home ademas de implementacion del onBackPressed en algunas actividades y confirmacion de cerrar app, home hecho, scroll añadiado al home, añadido crear trade

--- app/src/main/java/com/example/diariodetradingapp/CreateTradeActivity.java
+++ app/src/main/java/com/example/diariodetradingapp/CreateTradeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.example.diariodetradingapp.databinding.ActivityCreateTradeBinding;
 import com.example.diariodetradingapp.databinding.ActivityHome2Binding;
@@ -16,6 +17,32 @@ public class CreateTradeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityCreateTradeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        
+        initializaSpinners();
+    }
+
+    private void initializaSpinners() {
+        ArrayAdapter<CharSequence> adapterState = ArrayAdapter.createFromResource(this, R.array.selectState, android.R.layout.simple_spinner_item);
+        binding.spEstateCreateTrade.setAdapter(adapterState);
+
+        ArrayAdapter<CharSequence> adapterEntry = ArrayAdapter.createFromResource(this, R.array.selectEntry, android.R.layout.simple_spinner_item);
+        binding.spEntryCreateTrade.setAdapter(adapterEntry);
+
+        ArrayAdapter<CharSequence> adapterYear = ArrayAdapter.createFromResource(this, R.array.selectYear, android.R.layout.simple_spinner_item);
+        binding.spYearCreateTrade.setAdapter(adapterYear);
+
+        ArrayAdapter<CharSequence> adapterDay = ArrayAdapter.createFromResource(this, R.array.selectDay, android.R.layout.simple_spinner_item);
+        binding.spDayCreateTrade.setAdapter(adapterDay);
+
+        ArrayAdapter<CharSequence> adapterMonth = ArrayAdapter.createFromResource(this, R.array.selectMonth, android.R.layout.simple_spinner_item);
+        binding.spMonthCreateTrade.setAdapter(adapterMonth);
+
+        ArrayAdapter<CharSequence> adapterMarket = ArrayAdapter.createFromResource(this, R.array.selectMarket, android.R.layout.simple_spinner_item);
+        binding.spMarketCreateTrade.setAdapter(adapterMarket);
+
+        ArrayAdapter<CharSequence> adapterEmotion = ArrayAdapter.createFromResource(this, R.array.selectEmotion, android.R.layout.simple_spinner_item);
+        binding.spEmotionCreateTrade.setAdapter(adapterEmotion);
+
     }
 
     @Override
